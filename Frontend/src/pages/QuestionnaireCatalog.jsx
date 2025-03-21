2c449f8e5423c9c0fd6e09eaf24e476551e3fcb2
Taras M <tmaliukh@gmail.com>
2025-03-21 15:46:18 +0200

    add no-cors

--- Frontend/src/pages/QuestionnaireCatalog.jsx
+++ Frontend/src/pages/QuestionnaireCatalog.jsx
@@ -13,12 +13,7 @@ export default function QuestionnaireCatalog() {
     try {
       setIsLoading(true);
       setError(null);
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/questionnaires?page=${page}&limit=9`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/questionnaires?page=${page}&limit=9`, { mode: 'no-cors' });
       
       if (!response.ok) {
         const data = await response.json();
