ee35d7c62e64c44b8a4761508c647695752942b1
Taras M <tmaliukh@gmail.com>
2025-03-21 15:41:15 +0200

    add headers

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,22 @@ const app = express();
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://eliftechfront.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, Accept, X-Requested-With');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  
+  res.setHeader('Access-Control-Max-Age', '3600');
+
   if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+    console.log('Handling OPTIONS request');
+    res.status(204).end();
+    return;
   }
-  
+
+  console.log(`${req.method} ${req.url}`, {
+    origin: req.headers.origin,
+    headers: req.headers
+  });
+
   next();
 });
 
